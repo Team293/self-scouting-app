e1f02a2db542f4598679d1486f8fb4552c37b401
Paridax <tgavdaly@gmail.com>
2023-03-14 16:35:23 -0400

    database updates

--- src/upload.js
+++ src/upload.js
@@ -8,17 +8,19 @@ class DatabaseConnector {
 
     async authenticate(email, password) {
         console.log("Authenticating...");
-        this.auth = await this.pb.admins.authWithPassword(email, password)
-        .catch((err) => {
-            console.log("Something went wrong. Likely invalid credentials.");
-        });
+        this.auth = await this.pb.admins
+            .authWithPassword(email, password)
+            .catch((err) => {
+                console.log(
+                    "Something went wrong. Likely invalid credentials."
+                );
+            });
     }
 
-    async export() {
-
-    }
+    async export() {}
 
     async generateMatch(match) {
+        const pb = this.pb;
         if (!this.auth) {
             console.log("Not authenticated. Please authenticate first.");
             return;
@@ -26,62 +28,144 @@ class DatabaseConnector {
 
         // Loop over each team and generate the teams necessary data
         const redAlliance = [];
-        console.log("LOADING RED ALLIANCE");
-        await match.redAlliance.robots.forEach(async (robot) => {
-            const teamId = robot.team;
-            // get team data from database
-            const record = await this.pb.collection('teams').getFirstListItem('teamNumber=' + teamId, { '$autoCancel': false })
-            .catch((err) => {
-                return null;
-            });
-
-            if (record !== null) {
-                console.log("Team found: " + teamId);
-                redAlliance.push(record);
-            } else {
-                console.log("Team not found: " + teamId);
-                console.log("Creating team...");
-                const team = {
-                    teamNumber: teamId,
-                    teamName: "Team " + teamId,
-                };
-                const record = await this.pb.collection('teams').create(team, { '$autoCancel': false });
-                redAlliance.push(record);
-            }
-        });
-
         const blueAlliance = [];
-        console.log("LOADING BLUE ALLIANCE");
-        await match.blueAlliance.robots.forEach(async (robot) => {
+
+        console.log("LOADING TEAMS...");
+        // Join the red alliance and blue alliance into one array
+        const teams = match.redAlliance.robots.concat(
+            match.blueAlliance.robots
+        );
+        for await (const robot of teams) {
             const teamId = robot.team;
             // get team data from database
-            const record = await this.pb.collection('teams').getFirstListItem('teamNumber=' + teamId, { '$autoCancel': false })
-            .catch((err) => {
-                return null;
-            });
-
-            if (record !== null) {
-                console.log("Team found: " + teamId);
-                redAlliance.push(record);
-            } else {
+            let record = null;
+            try {
+                record = await pb
+                    .collection("teams")
+                    .getFirstListItem("teamNumber=" + teamId);
+            } catch (err) {
                 console.log("Team not found: " + teamId);
                 console.log("Creating team...");
+
                 const team = {
                     teamNumber: teamId,
                     teamName: "Team " + teamId,
                 };
-                const record = await this.pb.collection('teams').create(team);
-                redAlliance.push(record);
+                record = await pb.collection("teams").create(team);
             }
-        });
-    } 
-}
 
+            if (robot.alliance.color === RED) {
+                redAlliance.push(record.id);
+            } else {
+                blueAlliance.push(record.id);
+            }
+        }
+
+        const matchData = {
+            date: new Date().toUTCString(),
+            redAlliance,
+            blueAlliance,
+            events: [],
+            matchType: match.matchType ?? undefined,
+            compType: match.compType ?? undefined,
+            matchNumber: match.matchNumber,
+        };
+
+        console.log("Creating match...");
+        console.log(redAlliance, blueAlliance);
+
+        const matchRecord = await pb
+            .collection("matches")
+            .create(matchData)
+            .catch((err) => {
+                throw new Error(
+                    "Failed to create match: " + match.matchNumber,
+                    err
+                );
+            });
+
+        const eventData = [];
+        console.log("LOADING EVENTS...");
+        for await (const [i, event] of match.events.entries()) {
+            // Get the field state at the time of the event
+            const fieldState = match.scoring.getFieldState(
+                match.events.slice(0, i)
+            );
+
+            console.log("Creating event...");
+            // Generate the event in the database
+            const eventRecord = await pb.collection("events").create({
+                type: event.type,
+                match: matchRecord.id,
+                fieldState,
+                event: {
+                    ...event,
+                    match: undefined,
+                    robot: event.robot.team,
+                    time: Math.round(event.time * 1000) / 1000,
+                },
+            });
+            // Add the event to the match list
+            eventData.push(eventRecord.id);
+        }
+
+        // Update the match with the events
+        await pb.collection("matches").update(matchRecord.id, {
+            events: eventData,
+        });
+    }
+
+    generateJSON(match) {
+        const date = new Date().toUTCString();
+        const redAlliance = match.redAlliance.robots.map((robot) => {
+            return robot.team;
+        });
+
+        const blueAlliance = match.blueAlliance.robots.map((robot) => {
+            return robot.team;
+        });
+
+        const events = match.events.map((event) => {
+            console.log(event);
+            return {
+                ...event,
+                robot: event.robot.team,
+                time: Math.round(event.time * 1000) / 1000,
+                match: undefined,
+            };
+        });
+
+        const matchType = match.matchType ?? undefined;
+        const compType = match.compType ?? undefined;
+        const matchNumber = match.matchNumber;
+
+        const matchData = {
+            date,
+            redAlliance,
+            blueAlliance,
+            events,
+            matchType,
+            compType,
+            matchNumber,
+        };
+
+        return JSON.stringify(matchData);
+    }
+
+    saveMatchToLocalStorage(match) {
+        const matchData = this.generateJSON(match);
+        const matchId = `MATCH-${match.compType}-${match.matchType}-${match.matchNumber}`;
+        localStorage.setItem(matchId, matchData);
+    }
+}
 
 async function generateTest(match) {
     const db = new DatabaseConnector();
-    await db.authenticate(document.getElementById("email").value, document.getElementById("password").value);
-    await db.generateMatch(match);
+    window.db = db;
+    await db.authenticate(
+        document.getElementById("email").value,
+        document.getElementById("password").value
+    );
 }
 
 window.generateTest = generateTest;
